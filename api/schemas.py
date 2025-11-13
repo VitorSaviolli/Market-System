2d8dfc1f7bfe55e2c88579770b6296f662fa6f28
VitorSaviolli <vitorsgonsalez@gmail.com>
2025-11-12 23:55:14 -0300

    Product Window em produção, OBS: decide fazer a parte de API e pular a UI pelo seu trabalho

---
+++ api/schemas.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, Field
+from typing import Optional,List
+from datetime import datetime
+
+# ProductBase herda de BaseModel
+# Isso torna ProductBase um schema Pydantic
+# Ganha automaticamente:
+# Validação de tipos
+# Conversão automática de dados
+# Mensagens de erro detalhadas
+# Serialização JSON
+class ProductBase(BaseModel): #torna um schema Pydantic
+    #"atributo": tipo = Field(...,.,.,descricao="..") gt = greater, ge = greater or equal
+    # ... = Field(...) -> ... = Campo obrigatorio(Elipsis)
+    name: str = Field(...,min_length=1,max_length=200,description="Nome do Produto")
+    price: float = Field(...,gt=0,description="Preço deve ser maior que 0")
+    stock: int = Field(...,ge=0, description="Estoque deve ser maior ou igual a 0")
+    description: Optional[str] = Field(None, max_length=500, description="Descrição")
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductUpdate(BaseModel): #Nao herda ProductBase para nao obrigar a trocar todos os campos
+    name: Optional[str] = Field(None, min_length=1, max_length=200)
+    price: Optional[float] = Field(None, gt=0)
+    stock: Optional[int] = Field(None,ge=0)
+    description: Optional[str] = Field(None, max_length=500)
+
+class ProductResponse(ProductBase): #herda de product base para pegar os campos
+    #Usado quando a API retonra dados de produtos(GET,POST,PUT)
+    id:int #isso para ProductBase possuir um ID | adiciona o campo id
+    #Nao foi feito o ID direto no ProductBase para evitar conflito com o Pydantic
+    class Config:
+        from_attributes = True #permite converter do SQLACLCHEMY (ORM)
+        
+class SaleItemCreate(BaseModel):
+    product_id: int = Field(...,gt=0,description="ID do produto")
+    quantity: int = Field(...,gt=0,description="Quantidade do produto")
+
+class SaleCreate(BaseModel):
+    """
+    {
+        "products": [
+            {"product_id": 1, "quantity": 2},
+            {"product_id": 2, "quantity": 1}
+        ],
+        "payment_method": "Dinheiro"
+    }
+    """
+     
+    products: List[SaleItemCreate] = Field(...,min_length=1, description="Lista de Produtos")
+    payment_method: str = Field(...,min_length=1,description="Metodo de Pagamento")
+
+class SaleResponse(BaseModel):
+    id: int
+    date: datetime
+    total: float
+    payment_method: str
+
+    class Config:
+        from_attributes = True
+    
--- ui/product_window.py
+++ ui/product_window.py
@@ -15,3 +15,38 @@ class ProductWindow:
         self.mode = mode
 
         self.select_product_id = None
+
+        self.create_widgets()
+        
+        if mode == "list":
+            self.load_products()
+
+    def center_window(window, width, height):
+        window.update_idletasks()
+        x = (window.winfo_screenwidth() // 2) - (width // 2)
+        y = (window.winfo_screenheight() // 2) - (height // 2)
+        window.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
+
+    #func para criar elementos principais na tela
+    def create_widges(self):
+        main_frame = ttk.Frame(self.window, padding="20")  #Espaçamento de 20px
+        main_frame.grid(row=0,column=0,stick=(tk.W,tk.E,tk.N,tk.S))
+
+        title = ttk.Label(
+            main_frame,
+            text="Cadastro de Produto",
+            font=("Arial", 18, "bold")
+        )
+        
+        title.grid(row=0,column=0, columnspan=2, pady=20) #ocupa duas colunas
+        #form de cadastro
+        form_frame = ttk.LabelFrame(main_frame,text="Dados do Produto",padding=10)
+        form_frame.grid(row=1, column=0, columnspan=2, stick=(tk.W,tk.E),pady=10)
+        
+        #campo:nome
+        ttk.Label(form_frame, text="Nome").grid(row=0,column=0,stick=(tk.W),pady=5)
+        self.name_entry = ttk.Entry(form_frame, width=40)
+        self.name_entry.grid(row=0, column=1, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+        #campo:estoque
+        ttk.Label(form_frame,text="Estoque:").grid(row=0,column=0,sticky=(tk.W, tk.E, tk.N, tk.S))
